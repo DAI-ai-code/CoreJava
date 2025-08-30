f2243fae430be5dbe8c38bfafb6ff52e049a44de
DAI-ai-code <ajcs2508@gmail.com>
2025-08-30 11:27:01 +0530

    21

--- CoreJava/src/Assignment5/Anagram.java
+++ CoreJava/src/Assignment5/Anagram.java
@@ -4,18 +4,24 @@ public class Anagram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s1 = "Silent";
-		String s2 = "Listen";
-		int sum1=0,sum2 = 0;
+		String s1 = "ccf";
+		String s2 = "ddd";
+		char[] ch = new char[26];
+		
 		for(int i=0; i<s1.length(); i++) {
-			sum1 += s1.toLowerCase().charAt(i);
-			sum2 += s2.toLowerCase().charAt(i);
+			ch[s1.charAt(i)-'a']++;
+			ch[s2.charAt(i)-'a']--;
 		}
-		if(sum1 == sum2) {
-			System.out.println("They are anagram");
-		}else {
-			System.out.println("They are not");
+		boolean isAnagram = true;
+		for(int i=0; i<26; i++) {
+			if(ch[i] != 0) {
+				isAnagram = false;
+				break;
+			}
 		}
+		
+		System.out.println("Are these strings anagram? Ans: " + isAnagram);
+		
 	}
 
 }
